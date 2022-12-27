02d99afd80c936e093057c1372313cb07497c8bd
Eris <34255207+Erislandio@users.noreply.github.com>
2022-12-27 15:42:54 -0300

    ajustes  nossas lojas

--- src/lib/stripClient.ts
+++
@@ -1,8 +0,0 @@
-import Stripe from "stripe";
-
-export default new Stripe(
-  'sk_test_51MJH3yImByRpPslV0OfXKGgphf17MmrPBzojJ3LczZSRkO8yAcqfAhOveVjY1V1G1Z8e5Fa1njLJgFXuzy5HaJkH00hLUMChhr' as string,
-  {
-    apiVersion: "2022-11-15",
-  }
-);
